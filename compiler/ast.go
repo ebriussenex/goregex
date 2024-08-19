5d74b81dca2586beab023eb938c96ca4f61b5093
ebriussenex <ebriussenex@gmail.com>
2024-08-20 00:57:56 +0300

    feat: compile character literal and groups

--- compiler/ast.go
+++ compiler/ast.go
@@ -7,6 +7,11 @@ type (
 		compile() (head *fsm.State, tail *fsm.State)
 	}
 
+	CompositeNode interface {
+		Node
+		Append(node Node)
+	}
+
 	Group struct {
 		ChildNodes []Node
 	}
@@ -17,11 +22,28 @@ type (
 )
 
 func (g *Group) compile() (*fsm.State, *fsm.State) {
-	panic("unimplemented")
+	initialState := fsm.State{}
+	curTail := &initialState
+
+	for _, expression := range g.ChildNodes {
+		nextStateHead, nextStateTail := expression.compile()
+		curTail.Merge(nextStateHead)
+		curTail = nextStateTail
+	}
+
+	return &initialState, curTail
 }
 
 func (l CharacterLiteral) compile() (*fsm.State, *fsm.State) {
-	panic("unimplemented")
+	initialState := fsm.State{}
+	endState := &fsm.State{}
+
+	initialState.AddTransition(endState, func(input rune) bool {
+		return input == l.Character
+	})
+
+	return &initialState, endState
+
 }
 func (g *Group) Append(node Node) {
 	g.ChildNodes = append(g.ChildNodes, node)
