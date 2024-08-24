f9b1535ed241263d10cfdc04b751c541c6e3e675
ebriussenex <ebriussenex@gmail.com>
2024-08-24 04:33:55 +0300

    feat: add fuzzy testing

--- regex/regex_test.go
+++ regex/regex_test.go
@@ -5,6 +5,70 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	name  string
+	regex string
+	input string
+}
+
+func TestFSMWithGoStdLibCharacterLiterals(t *testing.T) {
+	nestedExpRegex := "a(b(c))"
+
+	testsCases := []testCase{
+		{"nested expressions", nestedExpRegex, "abcd"},
+	}
+
+	for _, tc := range testsCases {
+		t.Run(
+			tc.name, func(t *testing.T) {
+				compareWithGoStdLib(t, NewRegex(tc.regex), tc.regex, tc.input)
+			},
+		)
+	}
+}
+
+func TestCharacterLiteralRegex(t *testing.T) {
+	abcRegex := "abc"
+
+	testsCases := []testCase{
+		{"empty string", abcRegex, ""},
+		{"non matching string", abcRegex, "xxx"},
+		{"matching string", abcRegex, "abc"},
+		{"partial matching string", abcRegex, "ab"},
+		{"nested expressions", "a(b(c))", "abcd"},
+	}
+
+	for _, tc := range testsCases {
+		t.Run(
+			tc.name, func(t *testing.T) {
+				compareWithGoStdLib(t, NewRegex(tc.regex), tc.regex, tc.input)
+			},
+		)
+	}
+}
+
+func FuzzTesting(f *testing.F) {
+	abcRegex := "abc"
+	f.Add(abcRegex, "abc")
+	f.Add(abcRegex, "abcs")
+	f.Add(abcRegex, "tdd")
+	f.Add(abcRegex, "")
+
+	nestedExpRegex := "c(a(b))"
+	f.Add(nestedExpRegex, "cca")
+	f.Add(nestedExpRegex, "cab")
+	f.Add(nestedExpRegex, "cab")
+
+	f.Fuzz(func(t *testing.T, regex, input string) {
+		if _, err := regexp.Compile(regex); err != nil {
+			t.Skip()
+		}
+
+		compareWithGoStdLib(t, NewRegex(regex), regex, input)
+	})
+
+}
+
 func compareWithGoStdLib(t *testing.T, regex *regex, regexStr, input string) {
 	t.Helper()
 	actualResult := regex.MatchString(input)
